f93c18b2a28ae3ff0c046641715cef0732ed21fb
abraham_52 <abrahamaguirre731@gmail.com>
2022-08-22 09:29:44 -0500

    obtener Mayor

---
+++ obMayor.py
@@ -0,0 +1,23 @@
+# Ejercicio 1, obtener el numero mayor 
+# x e y son numeros enteros 
+#  Devuelve el numero más grande 
+# si son iguales , devuelve cualquier de los dos 
+#Tu codigo 
+a= int(input("primer numero : "))
+b = int(input("segundo  numero :"))
+
+def obMayor (a,b):
+      r = a,b
+      
+
+    
+if a < b:
+    print (a+"es menor  que "+b)
+# si la calificacion es menor a 8 el alumno presenta examen ordinario
+
+elif b > a:
+    print(b+" es  mayor que A"+ a)
+    # si la calificacion es mayor a 8 excentas 
+              
+              
+            
